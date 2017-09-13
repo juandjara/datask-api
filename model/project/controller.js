f2b9a1a24523762110287cb8c9a3d79571202700
Juan D. Jara <juanorigami@gmail.com>
2017-09-13 18:18:45 +0200

    feat: move pagination to generic controller

--- model/project/controller.js
+++ model/project/controller.js
@@ -2,15 +2,6 @@ const Controller = require('../../lib/controller');
 const projectFacade = require('./facade');
 
 class ProjectController extends Controller {
-  paginate(req, res, next) {
-    const {sort, page = 1, limit = 5} = req.query
-    return this.facade.paginate({}, {
-      sort,
-      page: parseInt(page),
-      limit: parseInt(limit)
-    }).then(page => res.json(page))
-      .catch(next)
-  }
 }
 
 module.exports = new ProjectController(projectFacade);
