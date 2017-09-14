8547973871b1e4fa27d7a9ea42e4395b27c3615d
Juan D. Jara <juanorigami@gmail.com>
2017-09-14 23:40:46 +0200

    feat: trying population

--- model/company/facade.js
+++ model/company/facade.js
@@ -1,6 +1,19 @@
 const Facade = require('../../lib/facade');
 const companySchema = require('./schema');
+const boom = require('boom')
+const objectIdValidator = require('valid-objectid')
 
-class CompanyFacade extends Facade {}
+class CompanyFacade extends Facade {
+  findById(...args) {
+    const id = args[0]
+    if (!objectIdValidator.isValid(id)) {
+      return Promise.reject(boom.badRequest('ObjectId is not valid'))
+    }
+    return this.Schema
+      .findById(...args)
+      .populate('users')
+      .exec();
+  }
+}
 
 module.exports = new CompanyFacade(companySchema);
--- model/user/facade.js
+++ model/user/facade.js
@@ -1,11 +1,23 @@
 const Facade = require('../../lib/facade');
 const userSchema = require('./schema');
+const boom = require('boom')
+const objectIdValidator = require('valid-objectid')
 
 class UserFacade extends Facade {
   findOneWithPassword(...args) {
     return this.Schema
       .findOne(...args)
       .select("+password")
+      .exec();
+  }
+  findById(...args) {
+    const id = args[0]
+    if (!objectIdValidator.isValid(id)) {
+      return Promise.reject(boom.badRequest('ObjectId is not valid'))
+    }
+    return this.Schema
+      .findById(...args)
+      .populate('company')
       .exec();
   }
 }
