54f5f273a457a102b4ecff8bbe2e65a76e1e2c95
Juan Domínguez <juan.dominguez@dinamicarea.com>
2017-10-17 12:37:58 +0200

    feat: project test boilerplate

---
+++ test/projects.test.js
@@ -0,0 +1,92 @@
+import test from 'ava'
+import request from 'supertest'
+import MongodbMemoryServer from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import User from '../model/user/schema'
+import Project from '../model/project/schema'
+
+mongoose.Promise = global.Promise
+process.env.NODE_ENV = 'test'
+const app = require('../server')
+const mongod = new MongodbMemoryServer()
+
+const createUsers = async () => {
+  const admin = new User({
+    email: 'admin',
+    password: 'admin',
+    name: 'admin',
+    roles: ["ADMIN"]
+  })
+  await admin.save()
+  const dev = new User({
+    email: 'dev',
+    name: 'dev',
+    password: 'dev',
+    roles: ["DEVELOPER"]
+  })
+  await dev.save()
+}
+const createProjects = async () => {
+  const opk = new Project({
+    name: "Opileak",
+    status: "ACTIVE"
+  })
+  await opk.save()
+}
+const login = async (user, pass) => {
+  const res = await request(app)
+    .post('/user/authenticate')
+    .send({email: user, password: pass})
+
+  return {res, token: res.body.token}
+}
+test.before(async () => {
+  const uri = await mongod.getConnectionString();
+  await mongoose.connect(uri, {useMongoClient: true});
+  await createUsers()
+  await createProjects()
+})
+test.after(() => {
+  mongod.stop()
+  mongoose.disconnect()
+})
+
+test(
+  'admin should be able to list all project',
+  async t => {
+    const {token} = await login('admin', 'admin')
+    const res = await request(app)
+      .get('/project')
+      .set('Authorization', `Bearer ${token}`)
+
+    const projects = res.body.docs
+    t.is(res.status, 200)
+    t.true(Array.isArray(projects))
+    t.is(typeof projects[0], 'object')
+    t.is(projects[0].name, 'Opileak')
+  }
+)
+test(
+  'developer should be able to list all project',
+  async t => {
+    const {token} = await login('dev', 'dev')
+    const res = await request(app)
+      .get('/project')
+      .set('Authorization', `Bearer ${token}`)
+
+    const projects = res.body.docs
+    t.is(res.status, 200)
+    t.true(Array.isArray(projects))
+    t.is(typeof projects[0], 'object')
+    t.is(projects[0].name, 'Opileak')
+  }
+)
+
+test.todo('admin should be able to get a project by id')
+test.todo('developer should be able to get a project by id')
+test.todo('admin should be able to create a new project')
+test.todo('developer should not be able to create a new project')
+test.todo('admin should be able to edit a project')
+test.todo('developer should not be able to edit a project')
+test.todo('admin should be able to delete a project')
+test.todo('developer should not be able to delete a project')
