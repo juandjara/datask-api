dc3b8ceec1ec820bf2113eb09bc9060f8ad9ade3
Juan D. Jara <juanorigami@gmail.com>
2017-11-22 21:11:20 +0100

    fix: time operations

--- model/time/facade.js
+++ model/time/facade.js
@@ -5,7 +5,11 @@ const userFacade = require('../user/facade');
 class TimeFacade extends Facade {
   create(body) {
     const time = new this.Schema(body)
-    return time.save()
+    return this.update(
+      {endTime: null, user: body.user},
+      {$set: {endTime: Date.now()}}
+    )
+    .then(() => time.save())
     .then(time => {
       userFacade.update(
         {_id: body.user},
@@ -23,8 +27,9 @@ class TimeFacade extends Facade {
       {_id: body.user},
       {$set: {activeTime: null}}
     )
-    const timeUpdate = this.Schema.findByIdAndUpdate(id, data)
+    const timeUpdate = this.findByIdAndUpdate(id, data)
     return Promise.all([timeUpdate, userUpdate])
+    .then(results => results[0])
   }
 }
 
